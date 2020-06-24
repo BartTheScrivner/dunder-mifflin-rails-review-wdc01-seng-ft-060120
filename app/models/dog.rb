f090d5b45a5e237f96bb8f3230b590eb44aa0e88
BartTheScrivner <dframe22@gmail.com>
2020-06-24 16:04:43 -0400

    Working index and show pages for both models

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,10 @@
 class Dog < ApplicationRecord
+  has_many :employees
+
+  def list_employees
+    emp_names = self.employees.map do |employee|
+      employee.to_s
+    end
+    emp_names.join(', ')
+  end
 end
